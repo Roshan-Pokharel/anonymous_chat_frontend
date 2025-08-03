a8b516337c2463842fd942abd382491d06b036bd
roshan <roshanpokharel890@gmail.com>
2025-08-03 14:37:52 +0545

    small change

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // Establish connection to the Socket.IO server
-const socket = io("http://localhost:3000");
+const socket = io("https://anonymous-chat-backend-1.onrender.com");
 
 // --- DOM Element Selectors ---
 const form = document.getElementById("form");
